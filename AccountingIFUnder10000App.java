1d4dfb5604f9caba6f004b8c50d6da56209b78d3
jongjin55 <44630719+jongjin55@users.noreply.github.com>
2020-10-23 15:19:20 +0900

    Accounting_Version2

---
+++ AccountingIFUnder10000App.java
@@ -0,0 +1,34 @@
+
+public class AccountingIFUnder10000App {
+
+	public static void main(String[] args) {
+		
+		
+		
+		double valueOfSupply = Double.parseDouble(args[0]);
+		double vateRate = 0.1;
+		double expenseRate = 0.3;
+		double vat = valueOfSupply*vateRate;
+		double total = valueOfSupply + vat;
+		double expense = valueOfSupply*expenseRate;
+		double income = valueOfSupply - expense;
+		
+		double dividend1 = income * 1.0;
+		double dividend2 = income * 0;
+		double dividend3 = income * 0;
+		
+		
+		System.out.println("Value of supply : " + valueOfSupply);
+		System.out.println("VAT : " + vat );
+		System.out.println("Total : " + total );
+		System.out.println("Expense : " + expense );
+		System.out.println("Income : " + income );
+		System.out.println("Dividend1 : " + dividend1 );
+		System.out.println("Dividend2 : " + dividend2);
+		System.out.println("Dividend3 : " + dividend3);
+		
+		
+		
+	}
+
+}
